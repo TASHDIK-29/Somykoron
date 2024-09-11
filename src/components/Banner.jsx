e367a3543510a78140eaacc3267a3d47578f5622
Tashdik <s2110477153@ru.ac.bd>
2024-09-12 02:30:42 +0600

    Updated headings text size

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,13 +3,13 @@
 
 const Banner = () => {
     return (
-        <div className="min-h-screen flex flex-col justify-center">
+        <div className="min-h-[60vh] lg:min-h-screen flex flex-col justify-center">
 
             <div className="flex flex-col justify-center">
-                <h1 className="text-center text-3xl lg:text-7xl font-semibold text-slate-300">
+                <h1 className="text-center text-4xl lg:text-7xl font-semibold text-slate-300">
                     Unlock Global Knowledge with
                     <br />Our <br />
-                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-cyan-500">Ai Translation Services</span>
+                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-cyan-500 text-3xl lg:text-7xl">Ai Translation Services</span>
                 </h1>
             </div>
             <p className="text-xl font-semibold text-center mt-9 text-slate-100">Accuracy of translation is 90%+ on most languages. Professional dubbing and subtitle creation</p>
